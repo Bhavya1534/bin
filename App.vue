a16a63033dd3e8f14c64aecafdcb40f54ac8c343
Bhavya1534 <139518528+Bhavya1534@users.noreply.github.com>
2023-10-02 14:17:25 +1100

    Update and rename 1.3 code.html to App.vue

---
+++ App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <CustomHeader />
+    <router-view />
+    <CustomFooter />
+  </div>
+</template>
+
+<script>
+import CustomHeader from './components/CustomHeader.vue';
+import CustomFooter from './components/CustomFooter.vue';
+
+export default {
+  components: {
+    CustomHeader,
+    CustomFooter,
+  },
+};
+</script>
+
